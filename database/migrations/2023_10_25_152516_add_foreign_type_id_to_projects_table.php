8d3d681e84bb035eb82205480a9b2aac5a1dbdb5
JordiMasci <133970995+JordiMasci@users.noreply.github.com>
2023-10-25 17:26:08 +0200

    fix key migration

--- database/migrations/2023_10_25_152516_add_foreign_type_id_to_projects_table.php
+++ database/migrations/2023_10_25_152516_add_foreign_type_id_to_projects_table.php
@@ -13,12 +13,8 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('types', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('description');
-            $table->date('date_of_birth');
-            $table->timestamps();
+        Schema::table('projects', function (Blueprint $table) {
+            $table->foreign('type_id')->after('id')->constrained();
         });
     }
 
@@ -29,6 +25,8 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('types');
+        Schema::table('projects', function (Blueprint $table) {
+            //
+        });
     }
 };
