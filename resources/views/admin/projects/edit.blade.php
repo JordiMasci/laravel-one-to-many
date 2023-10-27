23f51da4d39a6d6d4013aeeeb25cceff246aa9b0
JordiMasci <133970995+JordiMasci@users.noreply.github.com>
2023-10-27 16:49:21 +0200

    feat: select-> controller : edit e update

--- resources/views/admin/projects/edit.blade.php
+++ resources/views/admin/projects/edit.blade.php
@@ -19,8 +19,13 @@
             </div>
 
             <div class="col-12 my-4">
-                <label for="type_id" class="form-label">Tipo</label>
-                <select name="type_id" id="type_id"></select>
+                <label for="type_id" class="form-label ">Tipo</label>
+                <select name="type_id" id="type_id" class="@error('type_id') is-invalid @enderror">
+                    <option value="100000">Non categorizzato</option>
+                    @foreach ($types as $type)
+                        <option value="{{ $type->id }}">{{ $type->name }}</option>
+                    @endforeach
+                </select>
             </div>
 
             <div class="col-12 mb-4">
